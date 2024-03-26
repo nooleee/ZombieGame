886f9c46b6bd5128c32513b3d57b6b45f4e214c0
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-26 15:01:29 +0900

    add Boss : setShield 메소드 추가

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,23 +1,25 @@
 package zombie;
 
 public class Boss extends Zombie{
-	public Boss(int pos, int hp, int max) {
-		super(pos, hp, max);
-		// TODO Auto-generated constructor stub
-	}
+//	public Boss(int pos, int hp, int max) {
+//		super(pos, hp, max);
+//		// TODO Auto-generated constructor stub
+//	}
 
 	private int shield;
 	
-	public int getShield() {
-		return this.shield;
-	}
 
 	public Boss(int pos, int hp, int max, int shield) {
 		super(pos, hp, max);
 		this.shield = shield;
 	}
 	
+	public int getShield() {
+		return this.shield;
+	}
 	
-
+	public void setShield(int shield) {
+		this.shield = shield;
+	}
 
 }
