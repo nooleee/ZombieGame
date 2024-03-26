3bdb97fcf4f07522bec5b31604b1cc0d629b185e
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-26 17:31:22 +0900

    add ZombieGame : run 메소드 추가

---
+++ zombie/src/zombie/ZombieGame.java
@@ -0,0 +1,13 @@
+package zombie;
+
+public class ZombieGame {
+
+	public static void main(String[] args) {
+		ZombieGame.run();
+	}
+	
+	public static void run() {
+		Game.getInstance().run();
+	}
+
+}
