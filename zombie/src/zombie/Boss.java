0f4b37ec237a934d1c88a9a17f5dc4cb07389680
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-26 15:33:11 +0900

    add Boss : attack 메소드 추가

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -1,12 +1,9 @@
 package zombie;
 
-public class Boss extends Zombie{
-//	public Boss(int pos, int hp, int max) {
-//		super(pos, hp, max);
-//		// TODO Auto-generated constructor stub
-//	}
-
+public class Boss extends Unit{
 	private int shield;
+	private final int FULL_POWER = 1;
+	private int power;
 	
 
 	public Boss(int pos, int hp, int max, int shield) {
@@ -20,6 +17,25 @@ public class Boss extends Zombie{
 	
 	public void setShield(int shield) {
 		this.shield = shield;
+	}
+
+	@Override
+	void attack(Unit unit) {
+		Hero hero = (Hero) unit;
+		int rNum = getRan().nextInt(4) + 1;
+		if(rNum == FULL_POWER) {
+			System.out.println("보스의 필살기 발동 2배의 공격력");
+			power = getRan().nextInt(getMax()) + 1;
+			hero.setHp(hero.getHp() - power*2);
+			System.out.printf("보스가 %d의 공격력으로 공격 : 현재 HERO hp : %d", power*2, hero.getHp());
+		}
+		else {
+			System.out.println("보스의 일반 공격");
+			power = getRan().nextInt(getMax()) + 1;
+			hero.setHp(hero.getHp() - power);
+			System.out.printf("보스가 %d의 공격력으로 공격 : 현재 HERO hp : %d", power, hero.getHp());
+		}
+		
 	}
 
 }
