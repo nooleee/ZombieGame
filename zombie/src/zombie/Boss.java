6486b5bb1d1fa142960f1bf6892f9fa62fd9d5fb
nooleee <NOOL@DESKTOP-RADNT0G>
2024-03-26 17:22:14 +0900

    add & modify Boss : attack 메소드 수정, fullPowerAttack, nomalAttack 메소드 추가

--- zombie/src/zombie/Boss.java
+++ zombie/src/zombie/Boss.java
@@ -24,18 +24,31 @@ public class Boss extends Unit{
 		Hero hero = (Hero) unit;
 		int rNum = getRan().nextInt(4) + 1;
 		if(rNum == FULL_POWER) {
-			System.out.println("보스의 필살기 발동 2배의 공격력");
-			power = getRan().nextInt(getMax()) + 1;
-			hero.setHp(hero.getHp() - power*2);
-			System.out.printf("보스가 %d의 공격력으로 공격 : 현재 HERO hp : %d", power*2, hero.getHp());
+			fullPowerAttack(hero);
 		}
 		else {
-			System.out.println("보스의 일반 공격");
-			power = getRan().nextInt(getMax()) + 1;
-			hero.setHp(hero.getHp() - power);
-			System.out.printf("보스가 %d의 공격력으로 공격 : 현재 HERO hp : %d", power, hero.getHp());
+			nomalAttack(hero);
 		}
-		
+	}
+	
+	private void fullPowerAttack(Unit hero) {
+		System.out.println("보스의 필살기 발동 2배의 공격력");
+		power = getRan().nextInt(getMax()) + 1;
+		hero.setHp(hero.getHp() - power*2);
+		if(hero.getHp() <= 0) {
+			hero.setHp(0);
+		}
+		System.out.printf("보스가 %d의 공격력으로 공격 : 현재 HERO hp : %d", power*2, hero.getHp());
+	}
+	
+	private void nomalAttack(Unit hero) {
+		System.out.println("보스의 일반 공격");
+		power = getRan().nextInt(getMax()) + 1;
+		hero.setHp(hero.getHp() - power);
+		if(hero.getHp() <= 0) {
+			hero.setHp(0);
+		}
+		System.out.printf("보스가 %d의 공격력으로 공격 : 현재 HERO hp : %d", power, hero.getHp());
 	}
 
 }
